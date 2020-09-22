28c47cdb2949f230dfb73443060cc8f045d77638
Alexander Lindberg <alexanderlndbrg@gmail.com>
2020-09-22 22:51:52 +0200

    Fixed polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,9 +5,8 @@
 # Terminate already running bar instances
 killall -q polybar
 
-## Wait until the processes have been shut down
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-## Launch
-polybar main >>/tmp/polybar1.log 2>&1 &
-echo "polybar launched!"
+
+# Launch bar1 and bar2
+polybar -c ~/.config/polybar/config.ini main &
