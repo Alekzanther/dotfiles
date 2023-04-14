39b72d810ccdc94ba13486d4a8121f7ab9eba8f4
Alekzanther <alexanderlndbrg@gmail.com>
2023-04-14 16:29:46 +0200

    Reset to default nvchad custom examples

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -1,48 +1,20 @@
--- First read our docs (completely) then check the example_config repo
-
+---@type ChadrcConfig
 local M = {}
 
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
 M.ui = {
-  theme = "catppuccin",
+  theme = "onedark",
+  theme_toggle = { "onedark", "one_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
 }
 
-M.plugins = {
+M.plugins = "custom.plugins"
 
-  ["neovim/nvim-lspconfig"] = {
-      config = function()
-        require "plugins.configs.lspconfig"
-        require "custom.plugins.lspconfig"
-      end,
-  },
-  ["williamboman/mason.nvim"] = {
-   override_options = {
-      ensure_installed = {
-        -- lua stuff
-        "lua-language-server",
-        "stylua",
-
-        -- web dev
-        "css-lsp",
-        "html-lsp",
-        "typescript-language-server",
-        "prettierd",
-        "eslint_d",
-
-        -- docker
-        "dockerfile-language-server",
-
-        -- shell
-        "shfmt",
-        "shellcheck",
-      },
-    },
-  },
-  ["jose-elias-alvarez/null-ls.nvim"] = {
-      after = "nvim-lspconfig",
-      config = function()
-         require "custom.plugins.null-ls"
-      end,
-   }
-}
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
 
 return M
--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -1,3 +1,7 @@
-vim.opt.relativenumber = true
+-- local autocmd = vim.api.nvim_create_autocmd
 
-
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
