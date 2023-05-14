f3396f9f1f7f20e28d03349b4723173582454a8e
Alekzanther <alexanderlndbrg@gmail.com>
2023-05-14 21:51:58 +0200

    Change copilot bindings

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -5,5 +5,6 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
-
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-P>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 vim.opt.relativenumber = true
