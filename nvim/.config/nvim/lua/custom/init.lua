8cbeda15dddce62f3cdceb48bdd0ecf59e3206f4
Alekzanther <alexanderlndbrg@gmail.com>
2023-05-18 00:15:47 +0200

    Copilot and other fixes

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -6,5 +6,6 @@
 --   command = "tabdo wincmd =",
 -- })
 vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-P>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+-- vim.api.nvim_set_keymap("i", "<C-p>", '<Plug>(copilot#Accept)', { silent = true, expr = true })
 vim.opt.relativenumber = true
+vim.g.copilot_assume_mapped = true
--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -9,7 +9,11 @@ M.general = {
     ["<leader>j"] = { ":lua require('harpoon.ui').nav_file(1) <CR>", "harpoon: item 1", opts = { nowait = true } },
     ["<leader>k"] = { ":lua require('harpoon.ui').nav_file(2) <CR>", "harpoon: item 2", opts = { nowait = true } },
     ["<leader>l"] = { ":lua require('harpoon.ui').nav_file(3) <CR>", "Harpoon: item 3", opts = { nowait = true } },
+    ["<C-p>"] = { 'copilot#Accept("<CR>")', "Copilot accept", opts = { expr = true, silent = true, replace_keycodes = false } },
   },
+  i = {
+    ["<C-p>"] = { 'copilot#Accept("<CR>")', "Copilot accept", opts = { expr = true, silent = true, replace_keycodes = false} },
+  }
 }
 
 -- more keybinds!
