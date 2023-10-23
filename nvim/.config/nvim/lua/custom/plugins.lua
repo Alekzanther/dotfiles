be79a542b405d90aabe99748be4cd6ef1d2c8d59
Alekzanther <alexanderlndbrg@gmail.com>
2023-10-23 14:04:27 +0200

    Add Trouble plugin

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -77,6 +77,13 @@ local plugins = {
       },
     },
   },
+  "folke/trouble.nvim",
+   dependencies = { "nvim-tree/nvim-web-devicons" },
+   opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+   },
 }
 
 return plugins
