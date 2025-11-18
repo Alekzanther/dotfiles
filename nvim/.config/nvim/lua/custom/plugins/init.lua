340614fd321aab336518bde16a83d0db8b341f0e
Alekzanther <alexanderlndbrg@gmail.com>
2025-11-18 22:05:49 +0100

    Add nvimtree and copilot

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,56 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	-- Official GitHub Copilot - simplest and most reliable option
+	{
+		"github/copilot.vim",
+		event = "InsertEnter",
+	},
+	-- nvim-tree: Most popular file tree (8,182+ stars)
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			-- Disable netrw (recommended by nvim-tree)
+			vim.g.loaded_netrw = 1
+			vim.g.loaded_netrwPlugin = 1
+
+			require("nvim-tree").setup({
+				view = {
+					width = 30,
+					relativenumber = true,
+					number = true,
+				},
+				renderer = {
+					group_empty = true,
+					highlight_git = true,
+					icons = {
+						show = {
+							git = true,
+							folder = true,
+							file = true,
+							folder_arrow = true,
+						},
+					},
+				},
+				filters = {
+					dotfiles = false,
+				},
+			})
+
+			-- Open nvim-tree on startup
+			vim.api.nvim_create_autocmd("VimEnter", {
+				callback = function()
+					require("nvim-tree.api").tree.open()
+				end,
+			})
+
+			-- Ctrl+N to toggle file tree
+			vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { desc = "Toggle file tree", silent = true })
+		end,
+	},
+}
